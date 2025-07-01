d3c9514305e63556a66e2eb17fe5c61f40aff2f6
jayakumar <146693424+JayaKumar200@users.noreply.github.com>
2025-07-01 15:36:17 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,295 @@
+   //cardpage//
+// import { Button } from "@/components/ui/button"
+// import {
+//   Card,
+//   CardAction,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card"
+// import { Input } from "@/components/ui/input"
+// import { Label } from "@/components/ui/label"
+
+// const page = ()=> {
+//   return (
+//     <Card className="w-full max-w-sm">
+//       <CardHeader>
+//         <CardTitle>Login to your account</CardTitle>
+//         <CardDescription>
+//           Enter your email below to login to your account
+//         </CardDescription>
+//         <CardAction>
+//           <Button variant="link">Sign Up</Button>
+//         </CardAction>
+//       </CardHeader>
+//       <CardContent>
+//         <form>
+//           <div className="flex flex-col gap-6">
+//             <div className="grid gap-2">
+//               <Label htmlFor="email">Email</Label>
+//               <Input
+//                 id="email"
+//                 type="email"
+//                 placeholder="m@example.com"
+//                 required
+//               />
+//             </div>
+//             <div className="grid gap-2">
+//               <div className="flex items-center">
+//                 <Label htmlFor="password">Password</Label>
+//                 <a
+//                   href="#"
+//                   className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+//                 >
+//                   Forgot your password?
+//                 </a>
+//               </div>
+//               <Input id="password" type="password" required />
+//             </div>
+//           </div>
+//         </form>
+//       </CardContent>
+//       <CardFooter className="flex-col gap-2">
+//         <Button type="submit" className="w-full">
+//           Login
+//         </Button>
+//         <Button variant="outline" className="w-full">
+//           Login with Google
+//         </Button>
+//       </CardFooter>
+//     </Card>
+//   )
+// }
+
+// export default page
+
+
+
+
+
+
+
+
+
+
+
+
+
+// sonner
+
+// "use client"
+
+// import { toast } from "sonner"
+
+// import { Button } from "@/components/ui/button"
+// import './globals.css'
+
+// const page =()=>  {
+//   return (
+//     <Button
+//       variant="outline"
+//       onClick={() =>
+//         toast("Event has been created", {
+//           description: "Sunday, December 03, 2023 at 9:00 AM",
+//           action: {
+//             label: "Undo",
+//             onClick: () => console.log("Undo"),
+//           },
+//         })
+//       }
+//     >
+//       Show Toast
+//     </Button>
+//   )
+// }
+
+// export default page;
+
+
+//alter
+
+
+// import {
+//   AlertDialog,
+//   AlertDialogAction,
+//   AlertDialogCancel,
+//   AlertDialogContent,
+//   AlertDialogDescription,
+//   AlertDialogFooter,
+//   AlertDialogHeader,
+//   AlertDialogTitle,
+//   AlertDialogTrigger,
+// } from "@/components/ui/alert-dialog"
+// import { Button } from "@/components/ui/button"
+
+// const  page = ()=> {
+//   return (
+//     <AlertDialog>
+//       <AlertDialogTrigger asChild>
+//         <Button variant="outline">Show Dialog</Button>
+//       </AlertDialogTrigger>
+//       <AlertDialogContent>
+//         <AlertDialogHeader>
+//           <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+//           <AlertDialogDescription>
+//             This action cannot be undone. This will permanently delete your
+//             account and remove your data from our servers.
+//           </AlertDialogDescription>
+//         </AlertDialogHeader>
+//         <AlertDialogFooter>
+//           <AlertDialogCancel>Cancel</AlertDialogCancel>
+//           <AlertDialogAction>Continue</AlertDialogAction>
+//         </AlertDialogFooter>
+//       </AlertDialogContent>
+//     </AlertDialog>
+//   )
+// }
+
+// export default page;
+
+
+
+
+//drawer
+
+
+"use client"
+
+import * as React from "react"
+import { Minus, Plus } from "lucide-react"
+import { Bar, BarChart, ResponsiveContainer } from "recharts"
+
+import { Button } from "@/components/ui/button"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
+const data = [
+  {
+    goal: 400,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 239,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 349,
+  },
+]
+
+const page=()=> {
+  const [goal, setGoal] = React.useState(350)
+
+  function onClick(adjustment: number) {
+    setGoal(Math.max(200, Math.min(400, goal + adjustment)))
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant="outline">Open Drawer</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Move Goal</DrawerTitle>
+            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 pb-0">
+            <div className="flex items-center justify-center space-x-2">
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => onClick(-10)}
+                disabled={goal <= 200}
+              >
+                <Minus />
+                <span className="sr-only">Decrease</span>
+              </Button>
+              <div className="flex-1 text-center">
+                <div className="text-7xl font-bold tracking-tighter">
+                  {goal}
+                </div>
+                <div className="text-muted-foreground text-[0.70rem] uppercase">
+                  Calories/day
+                </div>
+              </div>
+              <Button
+                variant="outline"
+                size="icon"
+                className="h-8 w-8 shrink-0 rounded-full"
+                onClick={() => onClick(10)}
+                disabled={goal >= 400}
+              >
+                <Plus />
+                <span className="sr-only">Increase</span>
+              </Button>
+            </div>
+            <div className="mt-3 h-[120px]">
+              <ResponsiveContainer width="100%" height="100%">
+                <BarChart data={data}>
+                  <Bar
+                    dataKey="goal"
+                    style={
+                      {
+                        fill: "hsl(var(--foreground))",
+                        opacity: 0.9,
+                      } as React.CSSProperties
+                    }
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export default page;
